b0c2cb56908124b78b129777f0718c849cb69479
luanfelipeantunes <140464814+luanfelipeantunes@users.noreply.github.com>
2023-09-24 18:31:26 -0300

    primeira versão php

---
+++ cadastro.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <p></p>
+    </main>
+
+    <?php 
+        $nome = $_REQUEST['nome'];
+        $sobrenome = $_REQUEST['sobrenome'];
+        $email = $_REQUEST['email'];
+        $senha = $_REQUEST ['senha'];
+    ?>
+    
+</body>
+</html>
